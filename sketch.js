b64e3aa94a814f8031c135fe7b1126ffcb815e27
nawzaysfinah <nawzaysfinah@gmail.com>
2023-05-08 21:43:04 +0800

    audio visualization with p5.sound library

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var song;
+var amp;
+var button;
+
+var volhistory = [];
+
+function toggleSong() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+function preload() {
+  song = loadSound("hot-in-logika.mp3");
+}
+
+function setup() {
+  createCanvas(700, 700);
+  background(0);
+  button = createButton("hover to pause");
+  button.mouseMoved(toggleSong);
+  song.play();
+  amp = new p5.Amplitude();
+}
+
+function draw() {
+  background(0);
+  var vol = amp.getLevel();
+  volhistory.push(vol);
+  stroke(255);
+  noFill();
+  push();
+  var currentY = map(vol, 0, 1, height, 0);
+  translate(0, height / 2 - currentY);
+  beginShape();
+  for (var i = 0; i < volhistory.length; i++) {
+    var y = map(volhistory[i], 0, 1, height, 0);
+    vertex(i, y);
+  }
+  endShape();
+  pop();
+  if (volhistory.length > width - 50) {
+    volhistory.splice(0, 1);
+  }
+
+  stroke(255, 0, 0);
+  line(volhistory.length, 0, volhistory.length, height);
+  //ellipse(100, 100, 200, vol * 200);
+}
